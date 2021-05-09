ac60fc1975d9ecc02f3057b1ff5b802dc5f83f26
vanyakulinich <kulinichvanya@gmail.com>
2021-05-09 21:22:01 +0300

    feat: user service jsdoc examples added

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -12,28 +12,52 @@ class UserService {
   // Define db interactions models logic inside methods
   // There are few examples below
 
+  /**
+   * Get all users from database.
+   * @returns Array<User>
+   */
   async getAllUsers() {
     const users = await this.userModel.find({})
     return users
   }
 
+  /**
+   * Find user in database by id
+   * @param {String | Number} userId
+   * @returns User
+   */
   async getUserById(userId) {
     const user = await this.userModel.findOneById(userId)
     if (!user) throw new Error(ERROR_NAMES.notFound)
     return user
   }
 
+  /**
+   * Find user in database by email
+   * @param {String} email
+   * @returns User
+   */
   async getUserByEmail(email) {
     const user = await this.userModel.findOne({ email })
     if (!user) throw new Error(ERROR_NAMES.notFound)
     return user
   }
 
+  /**
+   * Create new user in database
+   * @param {Object} user
+   * @returns User
+   */
   async createUser(user) {
     const user = await this.userModel.create(user)
     return user
   }
 
+  /**
+   * Update user in database by id
+   * @param {Object: {userId: String, updateObj: Object}} Object
+   * @returns User
+   */
   async updateUserById({ userId, updateObj }) {
     // mongo example
     const user = this.userModel.findOneAndUpdate({ _id, userId }, { ...updateObj }, { new: true })
@@ -41,6 +65,10 @@ class UserService {
     return user
   }
 
+  /**
+   * Delete user by id
+   * @param {String | Number} userId
+   */
   async deleteUserById(userId) {
     // mongo example
     await this.userModel.deleteOne({ _id: userId })
