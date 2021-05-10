4107aa4c8e378e57bac1f08f65b27d03f1b55b9f
vanyakulinich <kulinichvanya@gmail.com>
2021-05-10 19:12:17 +0300

    feat: swagger docs config added

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -15,4 +15,17 @@ mainController.get("/test", (req, res) => {
  */
 mainController.use("/auth", authController).use("/users", usersController)
 
+/**
+ * Swagger Docs
+ * in case of exposing docs only in development, rely on process.env
+ *
+ * if(process.env.NODE_ENV === "development") {
+ *  setup swagger and use in controller
+ * }
+ */
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require("../../swagger.json")
+mainController.use("/docs", swaggerUi.serve)
+mainController.get("/docs", swaggerUi.setup(swaggerDocument))
+
 module.exports = mainController
